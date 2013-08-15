31d2bea219a0abcdd43a4a040684c2ede1fb95f4
Max Claus Nunes <maxcnunes@gmail.com>
2013-08-14 23:42:54 -0300

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rdstation::Application.config.secret_key_base = 'b506bd1e52c34f5b2d0f8b304a3148e2a8e221f0c10fde5494f872aa2281448e65d34da7cb9b9e96e342d51a5b7ba5f0a4539d919a18b8bd7de70d0f85639553'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rdstation::Application.config.session_store :cookie_store, key: '_rdstation_session'
